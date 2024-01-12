3290cac3239661dedc2ce47ab13cfe6e1b42dd90
Miguel Angel <miaguelvargas2540@gmail.com>
2024-01-12 15:24:14 -0500

    Estructura Fr

---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <HelloWorld msg="WE ARE MAKING A PROKECT"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
