ee629478e10955ab7958c22ba73b9fb00ae947ac
Bryan Alzate <bryanalzate007@gmail.com>
2024-01-21 23:08:25 -0500

    se carga nuevos archivos de Vue para pasar adelanto de Laura a Vue

---
+++ horizon/src/views/home.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="encabezado">
+      <img src="@/assets/logito.png" alt="Logo" class="logo">
+      <div class="enlaces">
+        <router-link to="/about">Acerca de nosotros</router-link>
+        <router-link to="/destinos">Destinos</router-link>
+        <router-link to="/politicas">Politicas</router-link>
+        <button id="botonLogin" class="boton-login" @click="irALogin">Login</button>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Home',
+    methods: {
+      irALogin() {
+        this.$router.push('/login');
+      }
+    }
+  }
+  </script>
+  
+  <style>
+  /* Tus estilos aquí */
+  </style>
+  
